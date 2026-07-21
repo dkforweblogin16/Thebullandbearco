d539ff3704e7e738aa6fae8457d1b9b10672bd13
Deepak <dkforweblogin16@gmail.com>
2026-07-21 10:18:30 +0530

    Reduce cookie max age to 1 hour

--- app/api/admin/elevate/route.js
+++ app/api/admin/elevate/route.js
@@ -40,7 +40,7 @@ export async function POST(request) {
     secure: true,
     sameSite: "lax",
     path: "/",
-    maxAge: 60 * 60 * 8, // 8 hours
+    maxAge: 60 * 60, // 1 hour
   });
   return res;
 }
@@ -55,4 +55,3 @@ export async function DELETE(request) {
   res.cookies.delete("admin_elevated");
   return res;
 }
-
